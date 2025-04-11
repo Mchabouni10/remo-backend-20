0974053de4458e1f9d6146464bab04221ea80a87
Mchabouni10 <mouloud619@gmail.com>
2025-04-11 10:15:26 -0500

    Update CORS to allow https://localhost

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-  credentials: true // If you're using tokens/cookies
+  origin: ['http://localhost:3000', 'https://localhost'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
 
 app.use(logger('dev'));
